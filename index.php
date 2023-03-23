6d9596f5577684fa1b4b02ae6ec793649e2fdf41
F3rn4n4d02 <107066961+F3rn4n4d02@users.noreply.github.com>
2023-03-23 13:29:57 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,39 +1,32 @@
 <?php
-error_reporting(0);
+error_reporting (0);
 if(isset($_POST['btn'])){
-
-   $numero0=POST['caja 1'];
-   $numero0=POST['caja 2'];
-   $numero1=(int)$numero1;
-   $numero2=(int)$numero2;
-   $resultado=$numero1+ $numero2;
-   $valor=$resultado;
-   echo $valor;
+    $numero1 = $_POST['Caja1'];
+    $numero2 = $_POST['Caja2'];
+    $numero1 = (int)$numero1;
+    $numero2 = (int)$numero2;
+    $resultado = $numero1 + $numero2;
+    $valor = $resultado;
+    echo $valor;
 }
-else{
-    echo "no existe datos";
-}
-
 ?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <title>practica1 con php</title>
 
+    <script src="js/jquery.js"></script>
+    <title>Practica1 con php</title>
 </head>
 <body>
-    
-   <!-- navar -->
+    <script src="js/bootstrap.min.js" ></script>
+    <!-- navbar -->
     <?php include "include/navbar.php" ?>
-    <?php include "include/menu.php" ?>
-    <?php include "include/operaciones.php" ?> 
-
-    <script scr="js/bootstrap.min.js"></script>
-    <script src="js/bootstrap.bundle.min.js"></script>
+    <?php  include "include/menu.php"?>
+    <?php include "include/menusu.php" ?>
+    
 </body>
 </html>
